a670c6ced117eb5d4b2fa3b23c304871c8d4efb4
kvacodes <kadiriavictor@gmail.com>
2024-05-26 09:24:46 +0100

    made some video changes

--- static/scripts/common.js
+++ static/scripts/common.js
@@ -12,28 +12,47 @@ document.addEventListener('DOMContentLoaded', function () {
         return fetch(url).then(response => response.blob());
     }
 
-    async function preloadVideos() {
-        [mobileVideoBlob, defaultVideoBlob] = await Promise.all([
-            fetchVideo(mobileVideoSrc),
-            fetchVideo(defaultVideoSrc)
-        ]);
+    async function preloadInitialVideo() {
+        const videoSrc = window.innerWidth <= 650 ? mobileVideoSrc : defaultVideoSrc;
+        const videoBlob = await fetchVideo(videoSrc);
 
-        // Initial video setup
-        setVideoSource();
-        // Hide the loading screen after videos are preloaded
+        // Set initial video source
+        const objectURL = URL.createObjectURL(videoBlob);
+        video_dyn.src = objectURL;
+
+        // Hide the loading screen
         loadingScreen.style.display = 'none';
+
+        // Preload the other video
+        preloadOtherVideo();
+    }
+
+    async function preloadOtherVideo() {
+        if (window.innerWidth <= 650) {
+            defaultVideoBlob = await fetchVideo(defaultVideoSrc);
+        } else {
+            mobileVideoBlob = await fetchVideo(mobileVideoSrc);
+        }
     }
 
     function setVideoSource() {
-        const videoBlob = window.innerWidth <= 650 ? mobileVideoBlob : defaultVideoBlob;
-        const objectURL = URL.createObjectURL(videoBlob);
-        video_dyn.src = objectURL;
+        let videoBlob;
+        if (window.innerWidth <= 650) {
+            videoBlob = mobileVideoBlob;
+        } else {
+            videoBlob = defaultVideoBlob;
+        }
+
+        if (videoBlob) {
+            const objectURL = URL.createObjectURL(videoBlob);
+            video_dyn.src = objectURL;
+        }
     }
 
-    // Sets the initial video source
-    preloadVideos();
+    // Preload the initial video
+    preloadInitialVideo();
 
-    // Updates the video source when the window is resized
+    // Update the video source when the window is resized
     window.addEventListener('resize', setVideoSource);
 
     // Adds an event listener to restart the video when it ends
