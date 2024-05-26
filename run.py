384e334cf94b554e80a5275fccd54e72f9cbaf3d
kvacodes <kadiriavictor@gmail.com>
2024-05-26 10:50:12 +0100

    creating pipeline to heroku

--- run.py
+++ run.py
@@ -195,5 +195,4 @@ def create_spotify_oauth():
 
 
 if __name__ == '__main__':
-    port = int(os.environ.get('PORT', 5000))
-    app.run(host='0.0.0.0', port=port)
+    app.run(debug=True)
--- static/scripts/common.js
+++ static/scripts/common.js
@@ -1,28 +1,65 @@
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener('DOMContentLoaded', function () {
     const video_dyn = document.getElementById('background-video');
+    const loadingScreen = document.getElementById('loading-screen');
 
-    function setVideoSource() {
+    // Preload videos
+    const mobileVideoSrc = 'static/videos/Background_Animation_mobile.mp4';
+    const defaultVideoSrc = 'static/videos/Background_Animation.mp4';
+
+    let mobileVideoBlob, defaultVideoBlob;
+
+    function fetchVideo(url) {
+        return fetch(url).then(response => response.blob());
+    }
+
+    async function preloadInitialVideo() {
+        const videoSrc = window.innerWidth <= 650 ? mobileVideoSrc : defaultVideoSrc;
+        const videoBlob = await fetchVideo(videoSrc);
+
+        // Set initial video source
+        const objectURL = URL.createObjectURL(videoBlob);
+        video_dyn.src = objectURL;
+
+        // Hide the loading screen
+        loadingScreen.style.display = 'none';
+
+        // Preload the other video
+        preloadOtherVideo();
+    }
+
+    async function preloadOtherVideo() {
         if (window.innerWidth <= 650) {
-            // Loads the mobile video source on smaller screens
-            video_dyn.src = 'static/videos/Background_Animation_mobile.mp4';
-            video_dyn.muted = true;
+            defaultVideoBlob = await fetchVideo(defaultVideoSrc);
         } else {
-            // Loads the default video source on larger screens
-            video_dyn.src = 'static/videos/Background_Animation.mp4';
-            video_dyn.muted = true;
+            mobileVideoBlob = await fetchVideo(mobileVideoSrc);
         }
     }
-    // Sets the initial video source
-    setVideoSource();
-    // Updates the video source when the window is resized
-    window.addEventListener('resize', setVideoSource);   
-    
+
+    function setVideoSource() {
+        let videoBlob;
+        if (window.innerWidth <= 650) {
+            videoBlob = mobileVideoBlob;
+        } else {
+            videoBlob = defaultVideoBlob;
+        }
+
+        if (videoBlob) {
+            const objectURL = URL.createObjectURL(videoBlob);
+            video_dyn.src = objectURL;
+        }
+    }
+
+    // Preload the initial video
+    preloadInitialVideo();
+
+    // Update the video source when the window is resized
+    window.addEventListener('resize', setVideoSource);
+
     // Adds an event listener to restart the video when it ends
-    const video = document.querySelector('video');
-    video.addEventListener('ended', function () {
-        if (video.muted !== false) {
-            video.currentTime = 0;
-            video.play();
+    video_dyn.addEventListener('ended', function () {
+        if (video_dyn.muted !== false) {
+            video_dyn.currentTime = 0;
+            video_dyn.play();
         }
     });
 });
