bbec007e40224b035b39df322fdad9e545d5ac80
kvacodes <kadiriavictor@gmail.com>
2024-05-26 08:01:21 +0100

    made some video changes

--- static/scripts/common.js
+++ static/scripts/common.js
@@ -1,28 +1,43 @@
 document.addEventListener('DOMContentLoaded', function() {
     const video_dyn = document.getElementById('background-video');
 
-    function setVideoSource() {
-        if (window.innerWidth <= 650) {
-            // Loads the mobile video source on smaller screens
-            video_dyn.src = 'static/videos/Background_Animation_mobile.mp4';
-            video_dyn.muted = true;
-        } else {
-            // Loads the default video source on larger screens
-            video_dyn.src = 'static/videos/Background_Animation.mp4';
-            video_dyn.muted = true;
-        }
+    // Preload videos
+    const mobileVideoSrc = 'static/videos/Background_Animation_mobile.mp4';
+    const defaultVideoSrc = 'static/videos/Background_Animation.mp4';
+
+    let mobileVideoBlob, defaultVideoBlob;
+
+    function fetchVideo(url) {
+      return fetch(url).then(response => response.blob());
     }
+
+    async function preloadVideos() {
+      [mobileVideoBlob, defaultVideoBlob] = await Promise.all([
+        fetchVideo(mobileVideoSrc),
+        fetchVideo(defaultVideoSrc)
+      ]);
+
+      // Initial video setup
+      setVideoSource();
+    }
+
+    function setVideoSource() {
+      const videoBlob = window.innerWidth <= 650 ? mobileVideoBlob : defaultVideoBlob;
+      const objectURL = URL.createObjectURL(videoBlob);
+      video_dyn.src = objectURL;
+    }
+
     // Sets the initial video source
-    setVideoSource();
+    preloadVideos();
+
     // Updates the video source when the window is resized
-    window.addEventListener('resize', setVideoSource);   
-    
+    window.addEventListener('resize', setVideoSource);
+
     // Adds an event listener to restart the video when it ends
-    const video = document.querySelector('video');
-    video.addEventListener('ended', function () {
-        if (video.muted !== false) {
-            video.currentTime = 0;
-            video.play();
-        }
+    video_dyn.addEventListener('ended', function () {
+      if (video_dyn.muted !== false) {
+        video_dyn.currentTime = 0;
+        video_dyn.play();
+      }
     });
-});
+  });
